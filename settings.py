033d99e80206db305d98ce04a576c9ce1199e6cc
unix-like <root@unix-like.com>
2017-05-30 19:12:53 +0800

    修复一个拼写错误

--- settings.py
+++ settings.py
@@ -13,4 +13,4 @@ class Settings():
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 255, 0, 0
-        self.bullet_allowed = 3
+        self.bullets_allowed = 3
