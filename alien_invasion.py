ac26afc4d616f3ae0c26d3162ae74abc90038b11
unix-like <root@unix-like.com>
2017-06-01 14:32:25 +0800

    创建一群外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,7 +4,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
-from  alien import Alien
+#from  alien import Alien
 
 def run_game():
     # 初始pygame、设置和屏幕对象
@@ -18,15 +18,20 @@ def run_game():
     ship = Ship(ai_settings,screen)
     # 创建一个用于存储子弹的编组
     bullets = Group()
+    # 创建一个外星人编组
+    aliens = Group()
+
+    # 创建一个外星人群
+    gf.create_fleet(ai_settings,screen,aliens)
 
     # 创建一个外星人
-    alien = Alien(ai_settings,screen)
+    # alien = Alien(ai_settings,screen)
 
     # 开始游戏的主循环
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,screen,ship,alien,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
