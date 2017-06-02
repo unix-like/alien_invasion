63af6fcf457b10376a67d425a544e0e57d9cd3f8
unix-like <root@unix-like.com>
2017-06-02 16:11:51 +0800

    游戏结束

--- game_functions.py
+++ game_functions.py
@@ -145,18 +145,19 @@ def change_fleet_direction(ai_settings,aliens):
 
 def ship_hit(ai_settings,stats,screen,ship,aliens,bullets):
     """响应被外星人撞到的飞船"""
-    # 将ships_left 减1
-    stats.ships_left -= 1
+    if stats.ship_left > 0:
+        stats.ships_left -= 1
 
-    # 清空外星人列表和子弹列表
-    aliens.empty()
-    bullets.empty()
+        # 清空外星人列表和子弹列表
+        aliens.empty()
+        bullets.empty()
 
-    # 创建一群新的外星人,并将飞船放到屏幕底端中央
-    create_fleet(ai_settings,screen,ship,aliens)
-    ship.center_ship()
+        # 创建一群新的外星人,并将飞船放到屏幕底端中央
+        create_fleet(ai_settings,screen,ship,aliens)
+        ship.center_ship()
 
-    # 暂停
-    sleep(0.5)
-
+        # 暂停
+        sleep(0.5)
+    else:
+        stats.game_active = False
 
