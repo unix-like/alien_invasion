e0fdc13ea1be84e27af029d36b75ec64f519b478
unix-like <root@unix-like.com>
2017-06-02 17:56:01 +0800

    在屏幕上绘制按钮

--- game_functions.py
+++ game_functions.py
@@ -30,11 +30,11 @@ def check_events(ai_settings,screen,ship,bullets):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event,ai_settings,screen,ship,bullets)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
 
-def update_screen(ai_settings,screen,ship,aliens,bullets):
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环时都重绘屏幕
     screen.fill(ai_settings.bg_color)
@@ -44,10 +44,14 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
     ship.blitme()
     aliens.draw(screen)
 
+    # 如果游戏处于非活动状态，就绘制Play按钮
+    if not stats.game_active:
+        play_button.draw_button()
+
     # 让最近绘制的屏幕可见
     pygame.display.flip()
 
-def update_bullets(ai_settings,screen,ship,aliens,bullets):
+def update_bullets(ai_settings, screen, ship, aliens, bullets):
     """更新子弹的位置，并删除已消失的子弹"""
     # 更新子弹的位置
     bullets.update()
