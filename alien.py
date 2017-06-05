ab88233dfebb7d5444ead15ff79b7d51e5b7df7b
unix-like <root@unix-like.com>
2017-06-05 23:29:29 +0800

    游戏结束

--- alien.py
+++ alien.py
@@ -1,12 +1,13 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Alien(Sprite):
     """表示单个外星人的类"""
 
-    def __init__(self,ai_settings,screen):
+    def __init__(self, ai_settings, screen):
         """初始化外星人并设置其初始位置"""
-        super(Alien,self).__init__()
+        super(Alien, self).__init__()
         self.screen = screen
         self.ai_settings = ai_settings
 
@@ -21,15 +22,6 @@ class Alien(Sprite):
         # 存储外星人的准确位置
         self.x = float(self.rect.x)
 
-    def blitme(self):
-        """在指定位置绘制外星人"""
-        self.screen.blit(self.image,self.rect)
-
-    def update(self):
-        """向左或者向右移动外星人"""
-        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
-        self.rect.x = self.x
-
     def check_edges(self):
         """如果外星人位于屏幕边缘就返回True"""
         screen_rect = self.screen.get_rect()
@@ -38,3 +30,11 @@ class Alien(Sprite):
         elif self.rect.left <= 0:
             return True
 
+    def update(self):
+        """向左或者向右移动外星人"""
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
+        self.rect.x = self.x
+
+    def blitme(self):
+        """在指定位置绘制外星人"""
+        self.screen.blit(self.image, self.rect)
